072cfcb14c2bfc7d599923b8246fea244bb4e113
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2019-04-04 23:44:18 +0300

    complete update name dialog

--- components/dialogs/NameDialog.js
+++ components/dialogs/NameDialog.js
@@ -2,25 +2,42 @@ import React from "react";
 import { View } from "react-native";
 import DialogInput from "react-native-dialog-input";
 import { connect } from "react-redux";
-function NameDialog({ dialogVisibility, closeDialog, userData }) {
-  return (
-    <View>
-      <DialogInput
-        isDialogVisible={dialogVisibility}
-        title={"Eπεξεργασία Εθελοντή"}
-        message={"Όνομα"}
-        initValueTextInput={userData.name}
-        submitInput={inputText => {
-          this.sendInput(inputText);
-        }}
-        closeDialog={() => closeDialog()}
-      />
-    </View>
-  );
+import { changeName } from "../../store/actions/actions";
+class NameDialog extends React.Component {
+  handleSubmit = inputText => {
+    const dataPouStelnw = {
+      id: this.props.userData.id,
+      name: inputText
+    };
+    this.props.onChangeName(dataPouStelnw);
+    this.props.closeDialog();
+  };
+  render() {
+    const { dialogVisibility, closeDialog, userData } = this.props;
+    return (
+      <View>
+        <DialogInput
+          isDialogVisible={dialogVisibility}
+          title={"Eπεξεργασία Εθελοντή"}
+          message={"Όνομα"}
+          initValueTextInput={userData.name}
+          submitInput={inputText => {
+            this.handleSubmit(inputText);
+          }}
+          closeDialog={() => closeDialog()}
+        />
+      </View>
+    );
+  }
 }
-
 const mapStateToProps = state => ({
   user: state.loggedInVolunteerData
 });
+const mapDispatchToProps = dispatch => ({
+  onChangeName: userData => dispatch(changeName(userData))
+});
 
-export default connect(mapStateToProps)(NameDialog);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NameDialog);
--- store/actions/actions.js
+++ store/actions/actions.js
@@ -224,6 +224,35 @@ export const changeLocation = userData => {
       });
   };
 };
+
+export const changeName = userData => {
+  return async dispatch => {
+    var token = await AsyncStorage.getItem("token");
+    await fetch(`https://alxmrd.com/api/mobile/changename`, {
+      method: "POST",
+      headers: {
+        ...headers,
+        Authorization: "Bearer " + token
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(result => result.json())
+      .then(res => {
+        res.httpstatus === "error"
+          ? dispatch({
+              type: ISINVALID,
+              payload: res
+            })
+          : dispatch({
+              type: UPDATE_VOLUNTEER,
+              payload: res
+            });
+      })
+      .catch(error => {
+        alert(error, "SERVER error 500 ");
+      });
+  };
+};
 export const clearMessage = (dispatch, message) => {
   dispatch({
     type: CLEAR_ISINVALID,
