4e832786633d57d90310c07be9baf65227472568
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2019-04-04 16:09:47 +0300

    complete email dialog

--- components/dialogs/EmailDialog.js
+++ components/dialogs/EmailDialog.js
@@ -2,25 +2,43 @@ import React from "react";
 import { View } from "react-native";
 import DialogInput from "react-native-dialog-input";
 import { connect } from "react-redux";
-function EmailDialog({ dialogVisibility, closeDialog, userData }) {
-  return (
-    <View>
-      <DialogInput
-        isDialogVisible={dialogVisibility}
-        title={"Eπεξεργασία Εθελοντή"}
-        message={"Ε-mail"}
-        initValueTextInput={userData.email}
-        submitInput={inputText => {
-          this.sendInput(inputText);
-        }}
-        closeDialog={() => closeDialog()}
-      />
-    </View>
-  );
+import { changeEmail } from "../../store/actions/actions";
+class EmailDialog extends React.Component {
+  handleSubmit = inputText => {
+    const dataPouStelnw = {
+      id: this.props.userData.id,
+      email: inputText
+    };
+    this.props.onChangeEmail(dataPouStelnw);
+    this.props.closeDialog();
+  };
+  render() {
+    const { dialogVisibility, closeDialog, userData } = this.props;
+    return (
+      <View>
+        <DialogInput
+          isDialogVisible={dialogVisibility}
+          title={"Eπεξεργασία Εθελοντή"}
+          message={"Ε-mail"}
+          initValueTextInput={userData.email}
+          submitInput={inputText => {
+            this.handleSubmit(inputText);
+          }}
+          closeDialog={() => closeDialog()}
+        />
+      </View>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
   user: state.loggedInVolunteerData
 });
+const mapDispatchToProps = dispatch => ({
+  onChangeEmail: userData => dispatch(changeEmail(userData))
+});
 
-export default connect(mapStateToProps)(EmailDialog);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EmailDialog);
