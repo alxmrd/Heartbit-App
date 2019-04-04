421f9c6107f5c187d7c0a792f098a82b1337c445
Αλέξανδρος Μυρωνιδης <alxmrd@Alexandross-MacBook-Air.local>
2019-04-04 23:48:08 +0300

    complete surname dialog update

--- components/dialogs/SurnameDialog.js
+++ components/dialogs/SurnameDialog.js
@@ -2,25 +2,42 @@ import React from "react";
 import { View } from "react-native";
 import DialogInput from "react-native-dialog-input";
 import { connect } from "react-redux";
-function SurnameDialog({ dialogVisibility, closeDialog, userData }) {
-  return (
-    <View>
-      <DialogInput
-        isDialogVisible={dialogVisibility}
-        title={"Eπεξεργασία Εθελοντή"}
-        message={"Επώνυμο"}
-        initValueTextInput={userData.surname}
-        submitInput={inputText => {
-          this.sendInput(inputText);
-        }}
-        closeDialog={() => closeDialog()}
-      />
-    </View>
-  );
+import { changeSurname } from "../../store/actions/actions";
+class SurnameDialog extends React.Component {
+  handleSubmit = inputText => {
+    const dataPouStelnw = {
+      id: this.props.userData.id,
+      surname: inputText
+    };
+    this.props.onChangeSurname(dataPouStelnw);
+    this.props.closeDialog();
+  };
+  render() {
+    const { dialogVisibility, closeDialog, userData } = this.props;
+    return (
+      <View>
+        <DialogInput
+          isDialogVisible={dialogVisibility}
+          title={"Eπεξεργασία Εθελοντή"}
+          message={"Όνομα"}
+          initValueTextInput={userData.surname}
+          submitInput={inputText => {
+            this.handleSubmit(inputText);
+          }}
+          closeDialog={() => closeDialog()}
+        />
+      </View>
+    );
+  }
 }
-
 const mapStateToProps = state => ({
   user: state.loggedInVolunteerData
 });
+const mapDispatchToProps = dispatch => ({
+  onChangeSurname: userData => dispatch(changeSurname(userData))
+});
 
-export default connect(mapStateToProps)(SurnameDialog);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SurnameDialog);
